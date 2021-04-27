15f40fb6cd5e7e6fd2012bdb5abed0a7d9db88ee
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-04-28 01:57:34 +0900

    2本目 12345を7で割った余りを表示するプログラムを作成せよ。

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _210428_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int a =int.Parse(System.Console.ReadLine());
+            int b = int.Parse(System.Console.ReadLine());
+            Console.WriteLine(a+"%"+b+"="+a % b);
+
+        }
+    }
+}
