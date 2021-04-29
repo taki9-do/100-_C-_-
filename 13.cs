cacb4f390790f96fb27e3590c2e1d73b636f6a95
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-04-30 02:36:18 +0900

    13本目　整数値を入力させ、0から入力値まで数を1ずつ増やして表示するプログラムを作成せよ。

---
+++ 13.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _210430_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            for(int i = 0; i <= a; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
