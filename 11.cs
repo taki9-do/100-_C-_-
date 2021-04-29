1a07463343e433339794de865929f7e71bad5232
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-04-30 02:29:16 +0900

    11～12本目　整数値を入力させ、その値の回数だけHello World!を繰り返して表示するプログラムを作成せよ。

---
+++ 11.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _210430_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello World!";
+            int b = int.Parse(Console.ReadLine());
+            for(int i = 0; i < b; i++)
+            {
+                Console.WriteLine(a);
+            }
+            
+        }
+    }
+}
