61833b2694df4ef937f2a42809f7545f7b719b96
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-04-29 22:24:29 +0900

    7－8本目　整数値を入力させ、値が正であればpositiveと表示するプログラムを作成せよ。ただし0は正には含まない。

---
+++ 8.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _210429_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a > 0)
+            {
+                Console.WriteLine("Positive");
+            }
+            else if (a == 0)
+            {
+                Console.WriteLine("Zero");
+            }
+            else
+            {
+                Console.WriteLine("Negative");
+            }
+            
+        }
+    }
+}
