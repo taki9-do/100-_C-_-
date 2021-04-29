07b517d0787da90deec5471eb10a3ab645ef5783
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-04-30 03:01:27 +0900

    16 整数値を入力させ、入力値が0でなければ再度入力させ、0であれば終了するプログラムを作成せよ。

---
+++ 16.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _210430_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=1;
+            while (a != 0)
+            {
+                Console.WriteLine("input:");
+                a = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
