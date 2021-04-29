250f5d3d80a9c2d40f58d7e480803989bd542271
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-04-30 02:17:25 +0900

    ７本目　整数値を入力させ、値が0ならzero、0でなければnot zeroと表示するプログラムを作成せよ。

---
+++ 7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _210428_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            if (i == 0)
+            {
+                Console.WriteLine("Zero");
+            }
+            else
+            {
+                Console.WriteLine("Not Zero");
+            }
+
+        }
+    }
+}
