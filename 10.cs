34aacb6dcbfa8be61e2ecb4564d8db02b9bf99a1
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-04-30 02:21:34 +0900

    10本目　整数値を入力させ、その値を絶対値にして表示するプログラムを作成せよ。（できれば変数の値を絶対値に変えるようにせよ）

---
+++ 10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _210430_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a < 0)
+            {
+                a = -a;
+            }
+
+            Console.WriteLine(a);
+
+        }
+    }
+}
