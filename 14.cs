7afcdc11508c5320f6bece3b2a3a0c1337d0c54a
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-04-30 02:49:03 +0900

    14 整数値を入力させ、入力値から0まで数を1ずつ減らして表示するプログラムを作成せよ。

---
+++ 14.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _210430_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            for (int i = a; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
