cdafd930c732ee8fcdfb6de5a80cc73e048e8178
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-04-29 22:16:47 +0900

    4本目 整数値を入力させ、その入力値を3倍した計算結果を表示するプログラムを作成せよ。

---
+++ 4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _210429_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(3*a);
+        }
+    }
+}
