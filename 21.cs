b3529902be52afc544563f27e4e462447a9970d3
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 21:03:55 +0900

    21　整数値を入力させ、その値が5よりも大きく、かつ、20よりも小さければOKと表示するプログラムを作成せよ。

---
+++ 21.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _210501_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a>5)
+            {
+                if (a < 20)
+                {
+                    Console.WriteLine("OK!");
+                }
+            }
+            
+        }
+    }
+}
