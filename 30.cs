7896ebe2b5a36f8c17449dbb21e55dec8bb9f564
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 23:23:58 +0900

    No. 30 棒グラフ
    整数値を入力させ、その個数だけ*を表示するプログラムを作成せよ。入力値が0以下の値の場合は何も書かなくてよい。

---
+++ 30.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _210501_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("input:");
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
