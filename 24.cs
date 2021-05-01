c9fd8f8c1c64a30a605a05ca46f6d2abb28a6e7c
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 21:05:50 +0900

    No. 24 -10以上0未満、または、10以上
    整数値を入力させ、その値が-10以上0未満、または、10以上であればOK、そうでなければNGと表示するプログラムを作成せよ。

---
+++ 24.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _210501_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a < 0)
+            {
+                if (a >= -10) Console.WriteLine("OK");
+                else Console.WriteLine("NG");
+                
+            }
+            else if (a >= 10) Console.WriteLine("OK");
+            else Console.WriteLine("NG!");
+        }
+    }
+}
