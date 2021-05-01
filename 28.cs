95581057f1e8c8c751f448461cb20da3b1918c8a
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 23:22:54 +0900

    No. 28 nの階乗
    整数値を入力させ、その値の階乗を表示するプログラムを作成せよ。ただし、0以下の値を入力した場合は1と表示する。

---
+++ 28.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _210501_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a > 0)
+            {
+                int b = 1;
+                for (int i = 0; i < a; i++)
+                {
+                    b = b * (i + 1);
+                }
+                Console.WriteLine(b);
+            }
+            else Console.WriteLine(1);
+           
+        }
+
+    }
+}
