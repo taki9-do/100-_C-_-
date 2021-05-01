8abfcad80187b15d01271f277661ee4ccada25d3
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 20:32:34 +0900

    19　要素数5の整数型の配列を宣言し、すべての配列に対して順に入力された整数値を代入し、すべての要素の値を表示するプログラムを作成せよ。

---
+++ 19.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _210501_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            for(int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("input:"+(a[i] = int.Parse(Console.ReadLine())));
+                
+
+                
+            }
+            for (int j = 0; j < a.Length; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            
+        }
+    }
+}
