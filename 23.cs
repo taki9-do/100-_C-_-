29b3154b13a54ba7082b6f8e6717c72e2ad18f81
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 21:05:10 +0900

    23　-5以上10未満
    整数値を入力させ、その値が-5以上10未満であればOK、そうでなければNGと表示するプログラムを作成せよ。

---
+++ 23.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _210501_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a >= -5)
+            {
+                if (a < 10)
+                {
+                    Console.WriteLine("OK");
+
+                }
+                else
+                {
+                    Console.WriteLine("NG");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("NG");
+
+            }
+        }
+    }
+}
