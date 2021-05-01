84fc7625aaed4701fe6084cc642dd0c7bc432253
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 23:22:22 +0900

    No. 27 1からnまでの和
    整数値を入力させ、1からその値までの総和を計算して表示するプログラムを作成せよ。ただし、0以下の値を入力した場合は0と表示する。

---
+++ 27.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _210501_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a > 0)
+            {
+                int[] b = new int[a];
+
+                for (int i = 0; i < a; i++)
+                {
+                    b[i] = i + 1;
+                }
+
+                Console.WriteLine(b.Sum());
+            }
+            else Console.WriteLine("0");
+            
+        }
+    }
+}
