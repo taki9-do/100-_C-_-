4347ebf2580f5518c36a0772590ab28b75c9cf4d
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 21:04:30 +0900

    22　-10以下または10以上
    整数値を入力させ、その値が-10以下、または、10以上であればOKと表示するプログラムを作成せよ。

---
+++ 22.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _210501_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a <= -10)
+            {
+                Console.WriteLine("OK!");
+            }
+            else if (a >= 20)
+            {
+                Console.WriteLine("OK!");
+
+            }
+
+        }
+    }
+}
