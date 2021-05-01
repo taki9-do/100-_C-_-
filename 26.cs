61a80ef07a852c572c7af3f3dfa34518dae2c15d
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 23:21:42 +0900

    No. 26 switch-case
    整数値を入力させ、その値が1ならone、2ならtwo、3ならthree、それ以外ならothersと表示するプログラムをswicth-case文を使って作成せよ。

---
+++ 26.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _210501_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            switch(a)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                default:
+                    Console.WriteLine("others");
+                    break;
+
+            };
+                
+            
+        }
+    }
+}
