55523d0e887accba1160b4369306c3e04940f8fd
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 23:23:23 +0900

    No. 29 5つの合計
    整数値を5回入力させ、それらの値の合計を表示するプログラムを繰り返しを使って作成せよ。

---
+++ 29.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _210501_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            for(int i = 0; i < a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine(a.Sum());
+        }
+    }
+}
