17083ea9faaa4722323cc0431b5fd679700314c2
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 21:06:24 +0900

    No. 25 -10未満?、-10以上0未満?、0以上?
    整数値を入力させ、その値が-10未満ならrange 1、-10以上0未満であればrange 2、0以上であればrange 3、と表示するプログラムを作成せよ。

---
+++ 25.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _210501_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 0)
+            {
+                Console.WriteLine("range 3");
+
+            }
+            else if (a >= -10)
+            {
+                Console.WriteLine("range 2");
+
+            }
+            else
+            {
+                Console.WriteLine("range 1");
+
+            }
+            
+        }
+    }
+}
