18619edee7bb08f07b3ccb4ca649ca2d06c61390
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 21:03:12 +0900

    20　整数値を2つ入力させ、1つめの値を2つめの値で割った結果を表示し、続けてその結果に2つめの値を掛けた結果を表示するプログラムを作成せよ。計算はすべて整数型で行うこと（割り切れない場合は自動的に小数点以下が切り捨てられる）。

---
+++ 20.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _210501_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("input 1st:" + a);
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("input 2nd:" + b);
+
+
+            Console.WriteLine(a/b);
+            Console.WriteLine((a / b) * b);
+
+
+           
+        }
+    }
+}
