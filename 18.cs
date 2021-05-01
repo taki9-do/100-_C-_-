fdf8b053a95e9f2147b91aa2ebd79eac04469fe1
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 20:32:08 +0900

    18　要素数10の整数型の配列を宣言し、整数値を入力させ、すべての配列の要素を入力値として、すべての要素の値を表示するプログラムを作成せよ。

---
+++ 18.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _210501_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            int b = int.Parse(Console.ReadLine());
+            for(int i = 0; i < a.Length; i++)
+            {
+                a[i] = b;
+                Console.WriteLine(a[i]);
+            }
+
+            
+        }
+    }
+}
