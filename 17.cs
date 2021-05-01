18fb5c8e18cdf214a0b157b3c5bd731eda3e016d
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-01 20:31:30 +0900

    17　要素数10の整数型の配列を宣言し、i番目の要素の初期値をiとし、順に値を表示するプログラムを作成せよ。

---
+++ 17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _210501_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            for(int i = 0; i < a.Length; i++)
+            {
+                a[i] = i;
+                Console.WriteLine(a[i]);
+            }
+
+            
+        }
+    }
+}
