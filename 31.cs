e1a6ce1c9fa21e327896fdf38ce1c8088a4c4451
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-02 22:10:36 +0900

    No. 31 棒グラフ改
    整数値を入力させ、その個数だけ*を、5個おきに空白（スペース）を入れて表示するプログラムを作成せよ。入力値が0以下の値の場合は何も書かなくてよい。

---
+++ 31.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _210502_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input:");
+            int a = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= a; i++)
+            {
+                if (i == 1)
+                {
+                    Console.Write("*");
+                }
+                else if ((i % 5) == 1)
+                {
+                    Console.Write("\t"+"*");
+
+                }
+                else
+                {
+                    Console.Write("*");
+
+                }
+            }
+            
+        }
+    }
+}
