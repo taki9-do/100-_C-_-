ce15dd468d290ec93affb1c4739d7697a020c5ee
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-02 22:11:07 +0900

    No. 32 5の倍数でbar
    1から20まで順に表示するが、5の倍数の場合は数字の代わりにbarと表示するプログラムを作成せよ。

---
+++ 32.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _210502_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 21; i++)
+            {
+                if (i == 1)
+                {
+                    Console.WriteLine(i);
+
+                }
+                else if ((i % 5) == 0)
+                {
+                    Console.WriteLine("bar");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+        }
+    }
+}
