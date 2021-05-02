d8a6f34a4c9f3670db3bb5fcb4578bb337977185
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-02 22:12:06 +0900

    No. 34 入力値抜き改
    整数値を入力させ、1から9まで、入力値と入力値+1以外を表示するプログラムを作成せよ。入力値が9の場合は9のみ表示しない。

---
+++ 34.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _210502_34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input:");
+            int a = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++)
+            {
+                if ((i == a)|| (i ==a+1 ))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+        }
+    }
+}
