c9f84c4c344468b89f917cf25b34f15a933b3227
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-02 22:14:24 +0900

    No. 35~36 続・配列要素の参照
    35　{3, 7, 0, 8, 4, 1, 9, 6, 5, 2}で初期化される大きさ10の整数型配列を宣言し、整数値を入力させ、要素番号が入力値である配列要素の値を表示するプログラムを作成せよ。入力値が配列の要素の範囲外であるかどうかのチェックは省略してよい。
    36　{3, 7, 0, 8, 4, 1, 9, 6, 5, 2}で初期化される大きさ10の整数型配列を宣言し、整数値を2つ入力させ、要素番号が入力値である2つの配列要素の値の積を計算して表示するプログラムを作成せよ。入力値が配列の要素の範囲外であるかどうかのチェックは省略してよい。

---
+++ 35.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _210502_35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a= { 3, 7, 0, 8, 4, 1, 9, 6, 5, 2 };
+            Console.Write("input index1:");
+            int i = int.Parse(Console.ReadLine());
+            Console.Write("input index2:");
+            int j = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(a[i]+"*"+a[j]+"="+a[i]*a[j]);
+
+           
+            
+        }
+    }
+}
