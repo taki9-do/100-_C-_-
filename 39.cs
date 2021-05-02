d9c22ace6be773c61086be64c82ccbdc4d9ba480
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-02 22:15:54 +0900

    No. 39 もっと・配列要素の参照
    {3, 7, 0, 8, 4, 1, 9, 6, 5, 2}で初期化される大きさ10の整数型配列を宣言し、最初は参照する要素番号を0とする。この参照する要素番号の配列要素の値から次の要素番号の配列要素の値を引いた値を表示し、参照する要素番号を1増やす。この手順を9回繰り返すプログラムを作成せよ。（具体的にどのような手順かは実行例を見て考えよう。）

---
+++ 39.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _210502_39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 3, 7, 0, 8, 4, 1, 9, 6, 5, 2 };
+            int temp = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine(a[temp] - a[temp + 1]);
+                temp += 1; 
+                
+            }
+        }
+    }
+}
