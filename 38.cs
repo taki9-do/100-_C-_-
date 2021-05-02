8660dd3b313b3f08bcc002602c467431837e3406
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-02 22:15:28 +0900

    No. 38 さらに・配列要素の参照
    {3, 7, 0, 8, 4, 1, 9, 6, 5, 2}で初期化される大きさ10の整数型配列を宣言し、最初は参照する要素番号を0とし、この参照する要素番号の配列要素の値を表示し、次にその配列要素の値を次の参照する要素番号とし、この次の参照する要素番号の配列要素の値を表示し、さらにその配列要素の値を次の参照する要素番号とし、……を10回繰り返すプログラムを作成せよ。（具体的にどのような手順かは実行例を見て考えよう。）

---
+++ 38.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _210502_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 3, 7, 0, 8, 4, 1, 9, 6, 5, 2 };
+            int temp = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                
+                Console.WriteLine(a[temp]);
+                temp = a[temp];
+            }
+
+
+        }
+    }
+}
