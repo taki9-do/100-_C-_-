e76921109205aab6bb3a6377c26800de344c0b57
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-02 22:15:00 +0900

    No. 37 続々・配列要素の参照
    {3, 7, 0, 8, 4, 1, 9, 6, 5, 2}で初期化される大きさ10の整数型配列を宣言し、整数値を入力させ、要素番号が入力値の配列要素の値を参照し、さらにその参照した値を要素番号とする配列要素の値を参照して表示するプログラムを作成せよ。入力値が配列の要素の範囲外であるかどうかのチェックは省略してよい。

---
+++ 37.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _210502_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = { 3, 7, 0, 8, 4, 1, 9, 6, 5, 2 };
+            Console.Write("input index:");
+            int i = int.Parse(Console.ReadLine());
+            
+
+            int temp = a[i];
+
+            Console.WriteLine("value = "+ a[temp]);
+        }
+    }
+}
