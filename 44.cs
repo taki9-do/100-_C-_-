0f79382f16dd5e125cc2916ae37e3e0be7d69d39
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-05 20:55:01 +0900

    No. 44 通貨換算
    換算したい金額（円単位）と1ドル何円かを整数値で入力すると、入力した金額が何ドル何セントか計算して表示するプログラムを作成せよ。1セントは1/100ドルである。結果は整数値でよい（1セント未満の端数切り捨て）。

---
+++ 44.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _210505_44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            Console.Write("何円?:");
+            money = int.Parse(Console.ReadLine());
+            int rate_Dollar;
+            Console.Write("１ドルは何円?:");
+            rate_Dollar = int.Parse(Console.ReadLine());
+
+            int dollar = money / rate_Dollar;
+            int St = (money % rate_Dollar) * 100 / rate_Dollar;
+
+            Console.Write(money+"円は"+dollar+"ドル"+St+"セント");
+        }
+    }
+}
