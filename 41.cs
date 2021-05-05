5cbddcfdfa0d15af9e36ecf8b1f845dfdab1ae08
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-05 20:53:17 +0900

    No. 41 1桁の自然数?
    整数値を入力させ、その値が一桁の自然数かそうでないか判定するプログラムを作成せよ。

---
+++ 41.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _210504_41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input:");
+            int a = int.Parse(Console.ReadLine());
+
+            if ((a >= 0) && (a < 9))
+            {
+                Console.WriteLine(a+" is single figure");
+            }
+
+           
+            Console.WriteLine(a+"is not single figure");
+        }
+    }
+}
