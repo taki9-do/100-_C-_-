fb9647e337906f6187b80a22cb1820b55265bc71
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-05 20:52:41 +0900

    No. 40 even or odd
    整数値を入力させ、その値が偶数ならばeven、奇数ならばoddと表示するプログラムを作成せよ。

---
+++ 40.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _210504_40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
