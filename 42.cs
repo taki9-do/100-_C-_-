73cf8eedcf69f4627bca52d8ae99502f2a61f6b7
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-05 20:53:46 +0900

    No. 42 小さい順?
    整数値を3つ入力させ、それらの値が小さい順（等しくてもよい）に並んでいるか判定し、小さい順に並んでいる場合はOK、そうなっていない場合はNGと表示するプログラムを作成せよ。

---
+++ 42.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _210505_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[3];
+
+            Console.Write("input1:");
+            a[0] = int.Parse(Console.ReadLine());
+            Console.Write("input2:");
+            a[1] = int.Parse(Console.ReadLine());
+            Console.Write("input3:");
+            a[2] = int.Parse(Console.ReadLine());
+
+            if (a[0] <= a[1])
+            {
+                if (a[1] <= a[2])
+                {
+                    Console.WriteLine("OK");
+                }
+                else
+                {
+                    Console.WriteLine("NG");
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("NG");
+
+            }
+
+
+        }
+    }
+}
