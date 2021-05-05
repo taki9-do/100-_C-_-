83083d622312abcf039ef1b97e9d2b85e08c6200
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-05 20:54:21 +0900

    No. 43 2次方程式の解の判別
    2次方程式 ax^2 + bx + c = 0 （x^2はxの2乗の意味）の係数a, b, cを入力し、2次方程式の解が2つの実数解か、重解か、2つの虚数解かを判別するプログラムを作成せよ。

---
+++ 43.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _210505_43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] confi = new int[3];
+            int answer;
+            
+            Console.Write("input a:");
+            confi[0] = int.Parse(Console.ReadLine());
+            Console.Write("input b:");
+            confi[1] = int.Parse(Console.ReadLine());
+            Console.Write("input c:");
+            confi[2] = int.Parse(Console.ReadLine());
+
+            answer = (confi[1]*confi[1]) - 4 * confi[0] * confi[2];
+            
+
+            if (answer < 0)
+            {
+                Console.Write("2つの虚数解");
+            }
+            else if (answer == 0)
+            {
+                Console.Write("重解");
+
+            }
+            else
+            {
+                Console.Write("2つの実数解");
+
+            }
+
+
+        }
+    }
+}
