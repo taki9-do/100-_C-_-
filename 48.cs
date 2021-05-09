cd5becb7cbeb71bc8b727c70eb871e2c6f883dc7
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-09 23:53:46 +0900

    No. 48 繰り返し計算
    最初に2以上の整数値を入力し、次の規則で計算し、計算回数と計算結果を表示し、計算結果が1になるまで繰り返すプログラムを作成せよ。
    規則：ある値が偶数ならその値を1/2にする。奇数ならその値を3倍して1を足す。

---
+++ 48.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _210509_48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input number:");
+            int a = int.Parse(Console.ReadLine());
+            int count = 1;
+
+            while (a != 1)
+            {
+                if ((a % 2) == 0)
+                {
+                    a /= 2;
+                    Console.WriteLine(count + ":" + a);
+                }
+                else if ((a % 2) == 1)
+                {
+                    a = a * 3 + 1;
+                    Console.WriteLine(count + ":" + a);
+
+                }
+                count++;
+            }
+
+            
+
+        }
+    }
+}
