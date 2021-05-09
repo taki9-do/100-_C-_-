5be00dc2b2bbe2b3e5b489854523a7ac9b9fdf86
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-09 23:52:34 +0900

    No. 46 入場料
    神山美術館の入場料金は、一人600円であるが、5人以上のグループなら一人550円、20人以上の団体なら一人500円である。人数を入力し、入場料の合計を計算するプログラムを作成せよ。

---
+++ 46.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _210509_46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("人数：");
+            int people = int.Parse(Console.ReadLine());
+            int money = 600;
+
+            if (people >= 20)
+            {
+                money = 500;
+            }
+            else if (people<20 && people >= 5)
+            {
+                money = 550;
+            }
+
+
+            Console.WriteLine("料金："+money*people);
+        }
+    }
+}
