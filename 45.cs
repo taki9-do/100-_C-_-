c43447ff33c890a275042763acf2f15492462dd6
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-09 23:51:50 +0900

    No. 45 タクシー料金
    初乗り料金が1700mまで610円、それ以降は313mごとに80円のタクシーがある。このタクシーに乗った距離をm単位で入力し、料金を計算するプログラムを作成せよ。

---
+++ 45.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _210509_45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("距離：");
+            int distance = int.Parse(Console.ReadLine());
+            int money=610;
+
+            
+
+            if (distance>=0 && distance <= 1700)
+            {
+                Console.WriteLine("金額："+money);
+
+
+            }
+            else
+            {
+                distance -= 1700;
+
+                while (distance > 0)
+                {
+                    money += 80;
+                    distance -= 313;
+                }
+                Console.WriteLine("金額：" + money);
+
+            }
+
+        }
+    }
+}
