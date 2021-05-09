a774336e0e747dc437d901990d4024b16cf6cd06
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-09 23:53:10 +0900

    No. 47 値の入れ替え
    異なる整数値を2つ入力し、それぞれ別の変数に格納する。そして、それらの変数の値を入れ替えた後、それぞれの変数の値を表示するプログラムを作成せよ。単に順序を変えて表示するだけではダメ。必ず変数の値を入れ替えること。下の実行例の場合、まず変数aに2、bに5が入力された状態から、aの値が5、bの値が2になるように入れ替える。

---
+++ 47.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _210509_47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("input b:");
+            int b = int.Parse(Console.ReadLine());
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("a="+a+",b="+b);
+        }
+    }
+}
