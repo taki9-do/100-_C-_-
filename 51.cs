da7dee47a6974c6423ecd72962268a533a6cab82
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-11 01:38:07 +0900

    No. 51 お支払い
    指定した金額を100円玉と10円玉と1円玉だけで、できるだけ少ない枚数で支払いたい。金額を入力するとそれぞれの枚数を計算して表示するプログラムを作成せよ。

---
+++ 51.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _210511_51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input money:");
+            int money = int.Parse(Console.ReadLine());
+            int coin_100=0, coin_10=0, coin_1=0;
+
+            if (money >= 100)
+            {
+                coin_100 = money / 100;
+                money %=  100;
+                coin_10 = money / 10;
+                money %=  10;
+                coin_1 = money / 1;
+
+            }
+            else if(money >= 10)
+            {
+                coin_10 = money / 10;
+                money %=  10;
+                coin_1 = money / 1;
+
+            }
+            else if (money >= 1)
+            {
+                coin_1 = money / 1;
+            }
+            Console.Write("100円玉" + coin_100 + "枚,");
+            Console.Write("10円玉" + coin_10 + "枚,");
+            Console.Write("1円玉" + coin_1 + "枚");
+
+        }
+    }
+}
