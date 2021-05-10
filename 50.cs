18fe126f01661689be8f9b31820a678909ef8e49
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-11 01:37:29 +0900

    No. 50 foobar
    1から100までの値を繰り返しで表示するが、3の倍数の時はfoo、5の倍数の時はbarと数字の代わりに表示するプログラムを作成せよ。なお、3と5の両方の倍数の時はfoobarと表示される。

---
+++ 50.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _210511_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 101; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("foobar");
+
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("foo");
+
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("bar");
+
+                }
+                else
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+        }
+    }
+}
