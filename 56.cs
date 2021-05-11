14304693f7f36662bff579eb8bda547945f5b648
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-11 18:06:09 +0900

    No. 56 2進数変換
    0〜65535の整数値を入力させ、入力値を16桁の2進数に変換して表示するプログラムを作成せよ。

---
+++ 56.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _210511_56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input number:");
+            int number = int.Parse(Console.ReadLine());
+            int[] box = new int[16];
+
+            for(int i = 0; i < box.Length; i++)
+            {
+                box[i] = number % 2;
+                number /= 2;
+            }
+
+            for(int j = 15; j >= 0; j--)
+            {
+                Console.Write(box[j]);
+
+            }
+        }
+    }
+}
