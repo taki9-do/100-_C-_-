d1bfc630512851db3d4f2e070b26bf00d96a1190
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-11 18:05:40 +0900

    No. 55 夢想花again
    「とんで」を9回「まわって」を3回繰り返した後「まわる」と表示して改行する、を3回繰り返すプログラムを作成せよ。「とんで」「まわって」と3行文の繰り返しは必ず繰り返し構文を使うこと。

---
+++ 55.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _210511_55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for(int i = 0; i < 3; i++)
+            {
+                while (count != 9)
+                {
+                    Console.Write("とんで");
+                    count++;
+                }
+                count = 0;
+
+                while (count != 3)
+                {
+                    Console.Write("まわって");
+                    count++;
+                }
+                count = 0;
+                Console.WriteLine("まわる");
+
+            }
+        }
+    }
+}
