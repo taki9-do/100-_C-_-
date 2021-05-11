bdd737f90d41da85d03b9f433e37dd04cc618ea4
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-11 17:57:59 +0900

    No. 52 閏年
    西暦を入力したらその年が閏（うるう）年かどうかを判定するプログラムを作成せよ。なお、4で割り切れる年のうち、100で割り切れないか、400で割り切れる年は閏年である。

---
+++ 52.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _210511_52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input year:");
+            int year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                if (year % 400==0)
+                {
+                    Console.WriteLine(year + "年はうるう年である");
+
+                }
+                else if (year % 100 != 0)
+                {
+                    Console.WriteLine(year + "年はうるう年である");
+
+                }
+                else
+                {
+                    Console.WriteLine(year + "年はうるう年でない");
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(year + "年はうるう年でない");
+            }
+
+        }
+    }
+}
