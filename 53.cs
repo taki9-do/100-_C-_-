a83057f7495c6eaceeafe12e46f632e7aa0ff35e
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-11 17:58:36 +0900

    No. 53 素因数分解
    自然数の入力値を素因数分解して結果を表示するプログラムを作成せよ。

---
+++ 53.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _210511_53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input number:");
+            int input_number = int.Parse(Console.ReadLine());
+            int devide = 2;
+            int temp = input_number;
+            while (temp != 1)
+            {
+                if (temp % devide == 0)
+                {
+                    temp /= devide;
+                    Console.Write(devide+" ");
+
+                }
+                else
+                {
+                    devide += 1;
+                }
+            }
+        }
+    }
+}
