c5589a92879bb5131cd2142d5cf3dcdb906290f1
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-13 01:55:09 +0900

    No. 59 行列の和
    3x3行列の和を求めて表示するプログラムを作成せよ。行列の値は2次元配列で表現し、繰り返しを使って計算すること。
    3x3行列とは縦3つ、横3つの9つの要素(値)をひとまとめにして扱うものである。2つの3x3行列の和は次式のように、それぞれ同じ位置にある値を足したものとして計算できる。
    
    例えばa12という要素は、1行目2列目の要素という意味である。それぞれ同じ位置にある要素を足せばよい。
    なお、入力値は1行ずつ3つの値をスペースで区切って入力するようにするとよい。このためには、scanf("%d %d %d", &a[0][0], &a[0][1], &a[0][2]);のように書く(No. 57参照)。

---
+++ 59.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _210512_59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //行列a３＊３定義
+            int[][] mat_a = new int[3][];
+            for(int i = 0; i < 3; i++)
+            {
+                mat_a[i] = new int[3];
+            }
+
+            //行列b３＊３定義
+            int[][] mat_b = new int[3][];
+            for(int i = 0; i < 3; i++)
+            {
+                mat_b[i] = new int[3];
+            }
+
+            Console.WriteLine("１つめの行例");
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    mat_a[j][i] = int.Parse(Console.ReadLine());
+
+                }
+            }
+
+
+            Console.WriteLine("２つめの行例");
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    mat_b[j][i] = int.Parse(Console.ReadLine());
+                }
+
+            }
+
+            Console.WriteLine("和");
+            for (int j = 0; j< mat_b.Length; j++)
+            {
+                for (int i = 0; i < mat_b[0].Length; i++)
+                {
+                    if (i == mat_b[i].Length - 1)
+                    {
+                        Console.WriteLine(mat_a[j][i] + mat_b[j][i]);
+
+                    }
+                    else
+                    {
+                        Console.Write(mat_a[j][i] + mat_b[j][i]);
+                        Console.Write(" ");
+
+                    }
+                }
+            }
+
+
+            Console.WriteLine();
+        }
+    }
+}
