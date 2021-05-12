70fc79eb616c46887b46940c56e0667deeddd793
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-13 01:54:33 +0900

    No. 58 棒グラフ
    0以上の整数値を5つ入力させ、それぞれの値に対して、次の形式でグラフを描くプログラムを作成せよ。
    形式：左端に値を表示し、適切に空白を空けて":"を書く（:で揃えるためにタブ\tを使うとよい）。その後ろに値の数だけ*を描くが、5個おきに空白を１つ入れる。具体例は下記の実行例を参照すること。

---
+++ 58.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _210512_58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] box = new int[5];
+
+            for(int i = 0; i < box.Length; i++)
+            {
+                Console.Write("input data[" + i + "]:");
+                box[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for(int j = 0; j < box.Length; j++)
+            {
+                Console.Write(box[j] + "\t:");
+                for(int k = 1; k <= box[j]; k++)
+                {
+                    Console.Write("*");
+                    if (k % 5 == 0) 
+                    { 
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
