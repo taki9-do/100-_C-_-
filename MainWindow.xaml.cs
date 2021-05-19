359ceff71c4eefd2b692745ec255c9939dfd6730
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-20 03:03:45 +0900

    No. 61 正方形と内接円
    円の中心座標を入力させ、横600縦400のウィンドウを開き、入力した中心座標に半径80の円と、それに外接する正方形を描くプログラムを作成せよ。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _210520_61
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+
+            int X_point = 200;
+            int Y_point = 100;
+
+            Rectangle rectangle = new Rectangle()
+            {
+                Fill = Brushes.White,
+                Stroke = Brushes.Blue,
+                Width = 200,
+                Height = 200,
+            };
+
+            canvas.Children.Add(rectangle);
+            Canvas.SetLeft(rectangle, X_point);
+            Canvas.SetTop(rectangle, Y_point);
+
+            Ellipse circle = new Ellipse()
+            {
+                Fill = Brushes.White,
+                Stroke = Brushes.Blue,
+                Width = rectangle.Width,
+                Height = rectangle.Height,
+            };
+
+            canvas.Children.Add(circle);
+            Canvas.SetLeft(circle, X_point);
+            Canvas.SetTop(circle, Y_point);
+        }
+    }
+}
