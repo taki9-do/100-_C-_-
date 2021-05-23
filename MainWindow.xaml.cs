7ae9fd01b834911ccb8dd9c5f5abd4821b0588a1
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-05-23 17:28:38 +0900

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _210523_69
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+        }
+
+        private void btn_decide_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+
+            //パネルの数
+            int panel_num;
+            panel_num = int.Parse(txb_panel_num.Text);
+
+            for (int i = 0; i < panel_num; i++)
+            {
+                for (int j = 0; j < panel_num; j++)
+                {
+                    Rectangle panel = make_rectangle(panel_num);
+
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            panel.Fill = Brushes.Black;
+                            add_rectangle(panel, panel.Width * i, panel.Height * j);
+
+                        }
+                        else
+                        {
+                            add_rectangle(panel, panel.Width * i, panel.Height * j);
+
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                        {
+                            add_rectangle(panel, panel.Width * i, panel.Height * j);
+
+                        }
+                        else
+                        {
+                            panel.Fill = Brushes.Black;
+                            add_rectangle(panel, panel.Width * i, panel.Height * j);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private Rectangle make_rectangle(int i)
+        {
+            Rectangle rect = new Rectangle()
+            {
+                Fill = Brushes.Green,
+                //Stroke = Brushes.Blue,
+                Width = canvas.Width / i,
+                Height = canvas.Height / i,
+            };
+
+            return rect;
+        }
+
+        private void add_rectangle(Rectangle rectangle, double X_point, double Y_point)
+        {
+            canvas.Children.Add(rectangle);
+            Canvas.SetLeft(rectangle, X_point);
+            Canvas.SetTop(rectangle, Y_point);
+        }
+    }
+}
