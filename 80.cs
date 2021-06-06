ebefb719cf7eee7c8c0b32e2d04b03cea2345e02
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-06-07 00:34:49 +0900

    No. 80 互いに素 ☆
    2つの正の整数値を入力させ、互いに素であるか判定するプログラムを作成せよ。なお、2つの正の整数が互いに素とは、1以外に共通公約数を持たない関係のことである。
    
    ヒント：両方の値が割りきれる2より大きい整数が存在しないことを確認すればよい。

---
+++ 80.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _210606_80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("入力1：");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("入力2：");
+            int b = int.Parse(Console.ReadLine());
+
+            int divisor = 0;
+
+            for(int i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    if (b % i == 0)
+                    {
+                        divisor = i;
+                    }
+                }
+            }
+
+            if (divisor > 1)
+            {
+                Console.WriteLine("互いに素でない");
+            }
+            else if(divisor==1)
+            {
+                Console.WriteLine("互いに素である");
+
+            }
+        }
+    }
+}
