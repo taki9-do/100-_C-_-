17347ecb980eb813e34dc2d7b35450910aa666b5
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-06-13 18:19:17 +0900

    No. 82 パスカルの三角形 ☆
    パスカルの三角形とは、1段目は1のみ、2段目からは段の数だけ数字が並び、両端は1、その間は左上と右上の値を足した値、として作っていった数字の並びである（Wikipedia「パスカルの三角形」参照）。このパスカルの三角形を15段まで計算して表示するプログラムを作成せよ。ただし表示は左詰で値はスペースで区切って表示するのでよい（三角形に並べなくてもよい）。
    
    ヒント：2次元配列を使うとよい。両端でないi段目、左からj番目の値a[i][j]は、一つ上の段の値を使って計算できる。上の実行結果を見て考えるとよいだろう。

---
+++ 82.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _210609_82
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //段数
+            int step = int.Parse(Console.ReadLine());
+
+            int[][] pascal = new int[step][];
+            for(int i = 0; i < pascal.Length; i++)
+            {
+                pascal[i] = new int[i+1];
+            }
+
+            for (int i = 0; i < step; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    if (i == 0)
+                    {
+                        pascal[0][0] = 1;
+
+                    }
+
+                    if (i >= 1)
+                    {
+                        //上加算（要素番号末尾以外）
+                        if (j < pascal[i - 1].Length)
+                        {
+                            pascal[i][j] += pascal[i - 1][j];
+
+                        }
+                        //左上加算（要素番号0以外）
+                        if (j  >= 1)
+                        {
+                            pascal[i][j] += pascal[i - 1][j-1];
+                        }
+                    }
+                    Console.Write(pascal[i][j]+" ");
+
+                }
+                Console.WriteLine();
+
+
+            }
+
+        }
+    }
+}
