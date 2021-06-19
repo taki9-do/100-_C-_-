d9c64971f28d6909c12ce210c4a2f439a9fa3b47
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-06-20 00:39:04 +0900

    No. 83 じゃんけん5回勝負 ☆
    次の仕様のじゃんけんプログラムを作成せよ。
    ・人間は、グー、チョキ、パーをそれぞれ0、1、2の数字で入力する。
    ・コンピュータは乱数を使って出す手を選ぶ。乱数の使い方は演習資料の高度なテクニック集を見よ。
    ・5回勝負として、人間とコンピュータの勝った回数を数え、勝敗がつくたびに1回ずつ表示する。あいこは決着がつくまで再勝負。途中でどちらかが3勝しても、5回最後まで勝負を続ける。
    ・指定された範囲以外の値を入力したら負けにする
    
    ヒント：まず1回だけのじゃんけんプログラムを作り、それを元に5回勝負にするとよい。勝敗判定はすべての組み合わせを条件判定する方法でもよいが、値の関係を上手く使うと比較的簡単な計算で判定できる。

---
+++ 83.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace _210613_83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choise_you=0 ;
+            int count_youwin=0;
+            int choise_cpu=0 ;
+            int count_cpuwin = 0;
+
+
+            for(int i = 0; i < 5; i++)
+            {
+                bool flag_aiko = true;
+                bool flag_out = false;
+
+
+                //trueで繰り返す
+                while (flag_aiko == true)
+                {
+                    flag_aiko = false;
+                    //手の入力（自分）
+                    Console.Write("あなたの手を選んでください（グー0、チョキ1、パー2）");
+                    choise_you = int.Parse(Console.ReadLine());
+                    if (choise_you != 0 && choise_you != 1 && choise_you != 2)
+                    {
+                        flag_out = true;
+                        Console.WriteLine("そんな手はありません。あなたの負け");
+                        break;
+                    }
+
+                    //手の入力(cpu)
+                    Random rnd = new Random();
+                    choise_cpu = rnd.Next(0, 3);
+                    if (choise_cpu == 0)
+                    {
+                        Console.WriteLine("コンピュータはグー");
+                    }
+                    else if (choise_cpu == 1)
+                    {
+                        Console.WriteLine("コンピュータはチョキ");
+
+                    }
+                    else if (choise_cpu == 2)
+                    {
+                        Console.WriteLine("コンピュータはパー");
+
+                    }
+
+                    rnd = null;
+
+                    if (choise_cpu == choise_you)
+                    {
+                        flag_aiko = true;
+                        Console.WriteLine("あいこ！");
+
+                    }
+
+                }
+
+                if (flag_out == true)
+                {
+                    count_cpuwin++;
+                    flag_out = false;
+                }
+                else if ((choise_you - choise_cpu + 3) % 3 == 1)
+                {
+                    count_cpuwin++;
+                    Console.WriteLine("わたしのかち");
+                }
+                else if((choise_you - choise_cpu + 3) % 3 == 2)
+                {
+                    count_youwin++;
+                    Console.WriteLine("あなたのかち");
+                }
+
+                Console.WriteLine("あなた" + count_youwin + "勝、わたし"+count_cpuwin+"勝");
+
+
+            }
+
+
+        }
+    }
+}
