5723b1c15face710cfc9b98ea6d0dc964c6423be
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-06-20 19:11:41 +0900

    No. 85 石取りゲーム ☆☆
    最初に石の個数を入力し（10個以上とする）、二人のプレイヤーが交互に1〜3個ずつ石を取り、最後の1個を取った方が負けとなるゲームがある。このゲームプログラムを作成せよ。具体的には実行例を参照のこと。
    
    上記実行例中、1〜3以外の数字が入力された場合は再度入力させている。

---
+++ 85.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _210620_85
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //石の入力
+            Console.Write("石の数を入力してください（10以上）：");
+            int stone = int.Parse(Console.ReadLine());
+
+            //奇数；１のターン、偶数；２のターン
+            int count=0;
+
+            while (stone > 1)
+            {
+                count++;
+                if (count % 2 == 1)
+                {
+                    Console.WriteLine("プレイヤー１の番です");
+                }
+                else if (count % 2 == 0)
+                {
+                    Console.WriteLine("プレイヤー２の番です");
+                }
+                stone = pulled_stone(stone);
+            }
+
+            if (stone == 1)
+            {
+                if (count % 2 == 1)
+                {
+                    Console.WriteLine("プレイヤー１の勝ち");
+                }
+                else if (count % 2 == 0)
+                {
+                    Console.WriteLine("プレイヤー２の勝ち");
+                }
+            }
+            else if (stone <= 0)
+            {
+                if (count % 2 == 1)
+                {
+                    Console.WriteLine("プレイヤー１の反則まけ");
+                }
+                else if (count % 2 == 0)
+                {
+                    Console.WriteLine("プレイヤー２の反則まけ");
+                }
+            }
+
+        }
+
+        #region 石をとる操作
+        static int pulled_stone(int stone)
+        {
+            Console.Write("何個石をとる（1～3個）？");
+            int number = int.Parse(Console.ReadLine());
+            if (number > 3)
+            {
+                Console.WriteLine("4個以上はダメ。もう一回選んで");
+                return pulled_stone(stone);
+            }
+            stone -= number;
+            Console.WriteLine("石の数：" + stone);
+            return stone;
+        }
+        #endregion
+
+
+    }
+}
