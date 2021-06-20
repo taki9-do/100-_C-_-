fb38bcea23785b4fc78fa7a83b524fc0e14baed5
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-06-20 16:54:21 +0900

    No. 84 トランプを切る ☆☆
    トランプをランダムに順番を変えて表示するプログラムを作成せよ。トランプは4つのスート（マーク）、1〜13までの数字の52枚である。トランプを表現する配列を作り、適当に順序を入れ替えていけばよい。適当に順序を入れ替えるには、例えば2つの入れ替えるカードを乱数を使って選び、それらを入れ替える操作を何回も繰り返せばよい。
    
    ヒント：トランプを数値で表現するには、スートと数字を別々に配列にする方法、それらを2次元配列にする方法、0から51までの1次元配列として扱い最後に表示する際に計算でスートと数字を取り出す方法、などが考えられる。なお、2つの入れ替えるカードを選ぶ代わりに、最初の要素から最後の要素までを順番に選び、それぞれについてランダムに選んだ入れ替え先のカードと入れ替えることで、少ない回数で確実にランダムな順序とすることができる。

---
+++ 84.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace _210620_84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //トランプ生成
+            string[] trump = generate_trump();
+
+            //トランプシャッフル
+            trump = shuffle_trump(trump);
+
+            //トランプ表示
+            for(int i = 0; i < trump.Length; i++)
+            {
+                Console.WriteLine(trump[i]);
+            }
+        }
+
+        #region トランプ生成
+        static string[] generate_trump()
+        {
+            string[] trump = new string[52];
+            for(int i = 0; i < trump.Length; i++)
+            {
+                int index = i - (i / 13) * 13;
+                if (i / 13 == 0)
+                {
+                    
+                    if (index == 0)
+                    {
+                        trump[i] = "スペード" + "A";
+                    }
+                    else if (index == 10)
+                    {
+                        trump[i] = "スペード" + "J";
+                    }
+                    else if (index == 11)
+                    {
+                        trump[i] = "スペード" + "Q";
+                    }
+                    else if (index == 12)
+                    {
+                        trump[i] = "スペード" + "K";
+                    }
+                    else
+                    {
+                        trump[i] = "スペード" + (index+1);
+                    }
+
+                }
+                else if (i / 13 == 1)
+                {
+                    if (index == 0)
+                    {
+                        trump[i] = "ハート" + "A";
+                    }
+                    else if (index == 10)
+                    {
+                        trump[i] = "ハート" + "J";
+                    }
+                    else if (index == 11)
+                    {
+                        trump[i] = "ハート" + "Q";
+                    }
+                    else if (index == 12)
+                    {
+                        trump[i] = "ハート" + "K";
+                    }
+                    else
+                    {
+                        trump[i] = "ハート" + (index + 1);
+                    }
+                }
+                else if (i / 13 == 2)
+                {
+                    if (index == 0)
+                    {
+                        trump[i] = "ダイヤ" + "A";
+                    }
+                    else if (index == 10)
+                    {
+                        trump[i] = "ダイヤ" + "J";
+                    }
+                    else if (index == 11)
+                    {
+                        trump[i] = "ダイヤ" + "Q";
+                    }
+                    else if (index == 12)
+                    {
+                        trump[i] = "ダイヤ" + "K";
+                    }
+                    else
+                    {
+                        trump[i] = "ダイヤ" + (index + 1);
+                    }
+                }
+                else if (i / 13 == 3)
+                {
+                    if (index == 0)
+                    {
+                        trump[i] = "クラブ" + "A";
+                    }
+                    else if (index == 10)
+                    {
+                        trump[i] = "クラブ" + "J";
+                    }
+                    else if (index == 11)
+                    {
+                        trump[i] = "クラブ" + "Q";
+                    }
+                    else if (index == 12)
+                    {
+                        trump[i] = "クラブ" + "K";
+                    }
+                    else
+                    {
+                        trump[i] = "クラブ" + (index + 1);
+                    }
+                }
+            }
+            return trump;
+        }
+        #endregion
+
+        #region トランプシャッフル
+        static string[] shuffle_trump(string[] trump)
+        {
+            for (int i = 0; i< trump.Length; i++)
+            {
+                Random rnd = new Random();
+
+                string temp = trump[i];
+                int index = rnd.Next(0, 52);
+
+                trump[i] = trump[index];
+                trump[index] = temp;
+
+                
+            }
+            return trump;
+        }
+        #endregion
+
+    }
+}
