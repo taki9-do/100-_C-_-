c6997c87d905e67fd6b19680180a3a18e116a441
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-06-23 20:28:59 +0900

    No. 87 運命数 ☆☆
    カバラ数秘術という簡単な占いがある。誕生日を年（西暦）・月・日で表し、それぞれの数字を足し合わせる。合計した数字が10以上であれば、再びすべての桁の数字を足し合わせる。これを1桁の数字になるまで繰り返し、得られた数字を運命数とする。ただし、計算途中で11、22、33のようにゾロ目の数字になった場合は、それを運命数とする。
    例：2015年12月23日→2+0+1+5+1+2+2+3=16→1+6=7
    運命数を計算するプログラムを作成せよ。
    
    ヒント：1の桁の数字は10で割った余りである。10で割ると1の桁を捨てて、それぞれの桁を右にずらした値になる。10で割った値が0になるまでこれらを繰り返すことで、各桁の数字を取り出すことができる。

---
+++ 87.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _210623_87
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("誕生日をYYYYMMDDの形式で入力してください:");
+            int number = int.Parse(Console.ReadLine());
+
+            //YYYYMMDDの合計
+            int sum = sum_number(number);
+
+            //１桁になるまでループ
+            while (sum > 9)
+            {
+                //11、22...のゾロ目で脱出
+                if ((sum / 10) == (sum % 10))
+                {
+                    break;
+                }
+                sum = sum_number(sum);
+            }
+
+            //運命数
+            Console.WriteLine("運命数は" + sum);
+        }
+
+        static int sum_number(int number)
+        {
+            int sum=0;
+
+            //最後の1桁まで加算
+            while (number > 0)
+            {
+                sum += (number % 10);
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
