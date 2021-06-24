8ae723fa821f2ea9153c1d0c0e1f45061378bb49
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-06-25 02:04:22 +0900

    No. 88 big or small ☆
    コンピュータは1から99の数字をランダムに選ぶ（正解値と呼ぶ）。プレイヤは値を入力し、正解値と一致すればクリアとなり値を入力した回数を表示する。一致しなければ正解値が入力値より大きいか小さいかを表示する。この数当てゲームプログラムを作成せよ。

---
+++ 88.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _210625_88
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //正解値
+            Random rnd = new Random();
+            int number_output = rnd.Next(1, 100);
+
+            //入力値
+            int number_input = 0;
+
+            //カウンタ
+            int count = 0;
+
+            while (number_input != number_output)
+            {
+                //入力値
+                Console.WriteLine("数を入力:");
+                number_input = int.Parse(Console.ReadLine());
+
+                if (number_input == number_output)
+                {
+                    Console.WriteLine("正解！");
+                    break;
+
+                }
+                else if(number_input > number_output)
+                {
+                    Console.WriteLine("それより小さいよ！");
+
+                }
+                else if(number_input < number_output)
+                {
+                    Console.WriteLine("それより大きいよ！");
+
+                }
+                count++;
+            }
+
+            Console.WriteLine(count + "回でクリア");
+
+        }
+    }
+}
