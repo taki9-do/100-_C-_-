a86e5388de2a88cf30037e2b1cff925b40a9eeae
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-07-05 03:34:38 +0900

    No. 93 宇宙の人 ☆☆☆
    まず開始値と終了値をそれぞれ入力させる。次に、開始値から終了値まで順に、3の倍数と3のつく数字のときは頭に"aho"と付けて表示するプログラムを作成せよ。開始値と終了値の値の妥当性（例えば終了値の方が開始値よりも大きいか）チェックは省略してよい。
    
    ヒント：3のつく数字の判定は、どのような数字でも対応できるように、それぞれの桁の数字を取り出して3かどうかを判定する必要がある。No.87を参考にせよ。
    また、条件が複雑になる場合、次のような方法を使うとよい：まずある変数の値を0としておき、いずれかの条件が該当したらその変数の値を1にする。すべての条件チェックが終わったら、その変数の値が0か1かで処理を切り替える。このような条件の成否を記憶しておく変数をフラグ変数と呼ぶ。

---
+++ 93.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _210704_93
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int value_start = 0;
+            int value_end = 0;
+
+            Console.Write("START ;");
+            value_start = int.Parse(Console.ReadLine());
+            Console.Write("END ;");
+            value_end = int.Parse(Console.ReadLine());
+
+            //判定
+            show(value_start, value_end);
+        }
+
+        static void show(int value_start,int value_end)
+        {
+            //フラグ変数
+            bool flag = false;
+
+            for (int i = value_start; i <= value_end; i++)
+            {
+                if (flag == false)
+                {
+                    flag = show_multiple_of_3(i);
+                }
+
+                if (flag == false)
+                {
+                    flag = show_inspect_3(i);
+
+                }
+
+
+                Console.WriteLine(i);
+                flag = false;
+
+            }
+        }
+
+        //3の倍数判定
+        static bool show_multiple_of_3(int i)
+        {
+            bool flag = false;
+            if (i % 3 == 0 && (i != 0))
+            {
+                Console.Write("aho ");
+                flag = true;
+            }
+            return flag;
+        }
+
+        static bool show_inspect_3(int i)
+        {
+            bool flag = false;
+            //最後の1桁まで加算
+            while (i > 0)
+            {
+                if ((i % 10) == 3)
+                {
+                    Console.Write("aho ");
+                    flag = true;
+                    break;
+
+                }
+                i /= 10;
+            }
+            return flag;
+        }
+
+    }
+}
