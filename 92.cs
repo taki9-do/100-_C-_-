3f4ff089ff525586d9ce6719c6eaf69a70d7f295
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-07-04 22:24:33 +0900

    No. 92 世界の人 ☆
    1から50まで順に表示していくが、3の倍数と3のつく数字のときは頭に"aho"と付けて表示するプログラムを作成せよ。
    
    ヒント：50までの数字なので、3のつく数字は一の桁か十の桁のどちらかが3の場合となる。一の桁は10で割った余りで判定できる。十の桁は範囲を考えればよい。（次の92と同じ方法で判定してもよい）

---
+++ 92.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _210704_92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 51; i++)
+            {
+                if (i % 3 == 0 && (i != 0))
+                {
+                    Console.Write("aho ");
+
+                }
+                else if (i % 10 == 3)
+                {
+                    Console.Write("aho ");
+                }
+                else if ((i / 10) % 3 == 0 && i > 9)
+                {
+                    Console.Write("aho ");
+                }
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
