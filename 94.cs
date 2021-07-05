9d4bf17985fc76cffae814d6af1af8718a8431f1
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-07-06 02:48:36 +0900

    No. 94 hit and blow その1 ☆☆☆
    hit and blowという数当てゲームがある。出題者は0〜9999の範囲の数字を正解として選ぶ。次に、解答者は予想する数字を言う。出題者は同じ桁（位置）に同じ数字があればヒット、桁は違うが同じ数字があればブローとして数え、ヒットとブローの数を答える（3桁以下の数字は頭に0が付いているものとする）。例えば、次のようになる：
    正解:1234 予想:1234 → 4ヒット（＝クリア）
    正解:1234 予想:5678 → 0ヒット0ブロー
    正解:1234 予想:1892 → 1ヒット1ブロー
    正解:0034 予想:3400 → 0ヒット4ブロー
    正解:1222 予想:1234 → 2ヒット0ブロー（ヒットはブローより優先して判定する）
    正解:1112 予想:1221 → 1ヒット2ブロー
    
    このゲームを3回に分けて作ってみよう。まず、コンピュータは正解となる4つの数字をランダムに選ぶ（同じ数字を何回使ってもよいし、0で始まってもよい）。次に、プレイヤーに4桁の数字を入力させる。そして、ヒットの数を数え、表示するプログラムを作成せよ。
    
    ヒント：正解の数字は、4桁の数字を別々に選ぶのでよい。後の判定のために配列に格納しておくと都合が良い。
    プレイヤーの入力する数字は整数値で入力させ、No.87と同じ方法で各桁の数字を取り出して、別の配列に格納しておく。この時、逆順に（1の位から）数字を取り出せば、最初の桁が0であってもうまくいく。
    入力値の各桁の数字が取り出せたら、各桁の数字を比較して一致している個数をカウントすればよい。

---
+++ 94.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _210706_94
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //4桁数字入力
+            int input_number;
+            Console.Write("4桁の数字を入力：");
+            input_number = int.Parse(Console.ReadLine());
+
+            //正解値生成
+            int target_number = make_target_number();
+            Console.WriteLine(target_number);
+
+            //hitのカウント
+            int hit_count = count_hit(input_number,target_number);
+
+            Console.Write(hit_count);
+
+
+        }
+
+        #region 正解値生成
+        static int make_target_number()
+        {
+            int number = 0;
+
+            for(int i = 0; i < 4; i++)
+            {
+                Random rnd = new Random();
+                number = (number * 10) + rnd.Next(0, 10);
+            }
+            return number;
+
+        }
+        #endregion
+
+        static int count_hit(int input,int target)
+        {
+            int count = 0;
+
+            while (input > 0)
+            {
+                if ((input % 10) == (target % 10)) 
+                {
+                    count++;
+                }
+                input /= 10;
+                target /= 10;
+            }
+
+            return count;
+        }
+
+    }
+}
