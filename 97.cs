2fa4bddddcf38ff93e012b457f37021938e5a046
taki9-do <57524962+taki9-do@users.noreply.github.com>
2021-07-11 16:06:20 +0900

    No. 97 ビンゴその1 ☆☆☆
    ビンゴゲームのカードを作るプログラムを作ろう。縦5マス、横5マス、計25マスのそれぞれに1〜75までの数字をランダムに配置する※。同じ番号は2回以上配置しない。作成したカードは、実行例のようにタブ(\t)を開けて表示すること。
    ※一般的なビンゴカードは、縦の5列それぞれは1〜15、16〜30、31〜45、46〜60、61〜75のそれぞれの数字のうち5つずつを配置し、中央のマスはフリーとしているが、ここでは簡単のためにこのようにしている。余裕があれば一般的なカードを作ってるとよいだろう。
    
    ヒント：同じ番号を2回配置しないための方法としては、例えば75個の数字を配列に入れてランダムに順番をシャッフルし（No.84参照）、先頭から順番にカードに配置していけばよい。他にも方法は考えられる。カードは、No.98のために2次元配列で表現するのがよい。

---
+++ 97.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _210711_97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5*5のビンゴ配列宣言
+            int[,] bingo = new int[5,5];
+
+            //ビンゴ配列に値格納
+            bingo = generate_bingo();
+
+            //ビンゴ表示
+            show_bingo(bingo);
+
+        }
+
+        static int[,] generate_bingo()
+        {
+            int[,] box = new int[5, 5];
+            int[] array_1to75 = generate_array_1to75_mixed();
+
+            
+
+
+
+            for (int i = 0; i < box.GetLength(0); i++)
+            {
+                for(int j = 0;j< box.GetLength(0); j++)
+                {
+                    box[i, j] = array_1to75[i * box.GetLength(0) + j];
+                }
+                
+            }
+
+            return box;
+        }
+
+        static int[] generate_array_1to75_mixed()
+        {
+            int[] box = new int[75];
+
+            for(int i = 0; i < box.Length; i++)
+            {
+                box[i] = i + 1;
+            }
+
+            for (int i = 0; i < box.Length; i++)
+            {
+                Random rnd = new Random();
+
+                int temp = box[i];
+                int index = rnd.Next(0, 75);
+
+                box[i] = box[index];
+                box[index] = temp;
+            }
+
+            return box;
+
+        }
+
+        static void show_bingo(int[,] box)
+        {
+
+            for (int i = 0; i < box.GetLength(0); i++)
+            {
+                for (int j = 0; j < box.GetLength(0); j++)
+                {
+                    Console.Write(box[i,j].ToString()+"\t");
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
